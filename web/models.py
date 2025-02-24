807827927a9d924a7ea1268cf1d758f10acd64dd
Nick Ershov <nick11.ershov@gmail.com>
2025-02-24 19:10:57 +0300

    add models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Director(models.Model):
+    name = models.CharField(100)
+    birthday = models.DateField
+
+
+class Genre(models.Model):
+    name = models.CharField(50)
+
+
+class Movie(models.Model):
+    title = models.CharField(150)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    release_date = models.DateField
+    description = models.TextField
+    poster_url = models.CharField(150)
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.IntegerField
+    text = models.TextField
+    created_ad = models.DateTimeField
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField
+    created_ad = models.DateTimeField
