1cd8c6bf7c8d471f03224d62d8fdbec3297e821a
Nick Ershov <nick11.ershov@gmail.com>
2025-02-24 22:10:55 +0300

    registration form

--- web/views.py
+++ web/views.py
@@ -1,3 +1,34 @@
+from datetime import datetime
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+from web.forms import RegistrationForm
+
+
+User = get_user_model()
+
+
+def main_view(request):
+    year = datetime.now().year
+    return render(request, "web/main.html", {
+        "year": year
+        })
+    
+
+def registration_view(request):
+    form = RegistrationForm()
+    is_success = False
+    if request.method == 'POST':
+        form = RegistrationForm(data=request.POST)
+        if form.is_valid():
+            user = User(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email']
+            )
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+            is_success = True
+    return render(request, "web/registration.html", {
+        "form": form,
+        "is_success": is_success
+        })
